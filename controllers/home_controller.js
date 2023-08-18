c9499ce2aef020958170ad2d1746071550f4bdc2
devan <devansareen6@gmail.com>
2023-08-18 12:16:40 +0530

    added home and user ejs file

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,5 @@
 module.exports.home = function (req, res) {
-    return res.end('<h1>Express app is up for VARTACHAT</h1>')
+    return res.render('home', {
+        title: "HOME"
+    });
 }
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 8000;
 app.use('/', require('./routes'));
 
 app.set('view engine', 'ejs');
-app.set('views', '/.views');
+app.set('views', './views');
 
 app.listen(port, function (err) {
     if (err) {
