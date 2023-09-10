f736c0041e1609ceac5b2dfd4a0bc37b8d77fc22
devan <devansareen6@gmail.com>
2023-09-10 22:02:22 +0530

    added aa ajaz method to create a post form

---
+++ assets/js/home_posts.js
@@ -0,0 +1,56 @@
+{
+    let createPost = function () {
+        let newPostForm = $('new-post-form');
+
+        newPostForm.submit(function (e)){
+            e.preventDefault();
+
+            $.ajax({
+                type: 'post',
+                url: '/posts/create',
+                data: newPostForm.serialize(),
+                success: function (data) {
+                    let newPost = newPostDom(data.data.post);
+                    $('#posts-list-container>ul').prepend(newPost)
+                }, error: function (error) {
+                    console.log(error.responseText);
+                }
+            });
+        }
+    }
+    let newPostDom = function (post) {
+        return $(` <li id="post-${post._id}">
+            < div class="post-label" > Post</ >
+    <p>
+        
+            <div class="delete-container">
+                <small>
+                    <button class="delete-button"><a href="/posts/destroy/${post.id}">Delete</a></button>
+                </small>
+            </div>
+    
+
+                ${post.content}
+                    <br>
+                    <small>
+                        ${post.user.name}
+                    </small>
+    </p>
+    <div class="post-comments">
+            <form action="/comments/create" method="POST">
+                <input type="text" name="content" placeholder="Type Here to add comment..." required="required">
+                <input type="hidden" name="post" value="${post._id}">
+                <input type="submit" value="Add Comment">
+            </form>
+
+                <div class="post-comments-list">
+                    <div class="comment-label">Comments</div>
+                    <ul id="post-comments-${post._id}">
+                
+                    </ul>
+                </div>
+    </div>
+</li>`);
+    }
+    createPost();
+}
--- config/passport-local.js
+++ config/passport-local.js
@@ -4,39 +4,40 @@ const User = require('../models/user');
 
 // Authentication using Passport
 passport.use(new LocalStrategy({
-    usernameField :'email'
-},      
-    async function(email, password, done) { // Use an async function for asynchronous operations
+    usernameField: 'email',
+    passReqToCallback: true
+},
+    async function (req, email, password, done) { // Use an async function for asynchronous operations
         try {
             const user = await User.findOne({ email: email }); // Await the findOne operation
-            
+
             if (!user) {
-                console.log('Invalid User');
+                req.flash('error', "Invalid Username/Password");
                 return done(null, false);
             }
-            
+
             // Perform password comparison here, you've commented this out
             if (password !== user.password) {
-                console.log('Invalid password');
+                req.flash('error', "Invalid Username/Password");
                 return done(null, false);
             }
 
             return done(null, user);
 
         } catch (err) {
-            console.log('Error in finding user --> Passport');
+            req.flash('error', err);
             return done(err);
         }
     }
 ));
 
 // Serializing the user to decide which key is to be kept in the cookies
-passport.serializeUser(function(user, done) {
+passport.serializeUser(function (user, done) {
     done(null, user.id);
 });
 
 // Deserializing the user from the key in the cookies
-passport.deserializeUser(async function(id, done) {
+passport.deserializeUser(async function (id, done) {
     try {
         const user = await User.findById(id); // Await the findById operation
         return done(null, user);
@@ -47,7 +48,7 @@ passport.deserializeUser(async function(id, done) {
 });
 
 // Check if the user is authenticated
-passport.checkAuthentication = function(req, res, next) {
+passport.checkAuthentication = function (req, res, next) {
     if (req.isAuthenticated()) {
         return next();
     }
@@ -55,7 +56,7 @@ passport.checkAuthentication = function(req, res, next) {
 }
 
 // Set the authenticated user to res.locals
-passport.setAuthenticatedUser = function(req, res, next) {
+passport.setAuthenticatedUser = function (req, res, next) {
     if (req.isAuthenticated()) {
         res.locals.user = req.user; // Change req.User to req.user
     }
