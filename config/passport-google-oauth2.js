c6ae02fdb330ceaff1433dbcd7740c01e31d9ee9
devan <devansareen6@gmail.com>
2023-10-03 21:36:20 +0530

    commit

--- config/passport-google-oauth2.js
+++ config/passport-google-oauth2.js
@@ -1,9 +1,9 @@
 const passport = require('passport');
-const googleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 const crypto = require('crypto');
 const User = require('../models/user');
 
-passport.use(new googleStrategy({
+passport.use(new GoogleStrategy({
     clientID: '1038315613083-r46bgl12lpuge3gmmb3o5gjtnhtm2mq5.apps.googleusercontent.com',
     clientSecret: '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',
     callbackURL: 'http://localhost:8000/users/auth/google/callback',
@@ -17,20 +17,18 @@ passport.use(new googleStrategy({
                 return done(null, existingUser);
             }
 
-            console.log('Creating new user...');
-            const newUser = new User.create({
-                id: profile.id,
+            const newUser = new User({
                 name: profile.displayName,
-                email: profile.emails[0].value
-
+                email: profile.emails[0].value,
+                password: crypto.randomBytes(20).toString('hex')
             });
+
             await newUser.save();
             return done(null, newUser);
         } catch (error) {
-            return done(error, false)
+            return done(error, false);
         }
     }
 ));
-
 
 module.exports = passport;
